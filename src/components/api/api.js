61b9eb706d51ea946335a13fc71acb6338fcb478
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-08-08 09:54:27 +0500

    commit

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import axios from 'axios';
 
-const api = () => {
-  return (
-    <div>api</div>
-  )
-}
+export const API = axios.create({
+    baseURL:'https://testpsyedu.limsa.uz'
+})
 
-export default api
+API.interceptors.request.use(
+    (config)=>{
+        const token = localStorage.getItem("token")
+        if(token){
+            config.headers.Authorization= `Bearer ${token}`
+        }
+        return config;
+    },
+    (error)=>Promise.reject(error)
+)
+
+API.interceptors.response.use(
+    (response)=>response,
+    (error)=>{
+        return Promise.reject(error);
+        }
+)
+
